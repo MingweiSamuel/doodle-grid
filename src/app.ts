4c51b9609c094ff693a0680b339cab199acde03a
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-08-09 15:02:45 -0700

    wip

--- src/app.ts
+++ src/app.ts
@@ -67,29 +67,104 @@ document.addEventListener("DOMContentLoaded", function (_event) {
         //         ongoingTouches.push(newTouch.identifier);
         //     }
         // });
-        const mat = [
-            [0, 0, 1, 0],
-            [0, 0, 0, 1],
-            [100, -100, 1, 0],
-            [100, 100, 0, 1],
-        ];
-        const matInv = math.inv(mat);
+        // const mat = [
+        //     [0, 0, 1, 0],
+        //     [0, 0, 0, 1],
+        //     [100, -100, 1, 0],
+        //     [100, 100, 0, 1],
+        // ];
+        // const matInv = math.inv(mat);
 
+        // /// a c e
+        // /// b d f
+        // /// 0 0 1
+        // let transformMatrix6 = [1, 0, 0, 1, 0, 0];
+
+        // sc, ss, tx, ty
+        // scaled cos, scaled sin, translate x, translate y
+        let transform = [1, 0, 0, 0];
+
+        type GestureStart = { identifier: number, x: number, y: number };
+        const activeGestureStarts: GestureStart[] = [];
+        const findActiveGestureIdx = ({ identifier }: { identifier: number }): number => {
+            for (let i = 0; i < activeGestureStarts.length; i++) {
+                if (identifier === activeGestureStarts[i].identifier) {
+                    return i;
+                }
+            }
+            return -1;
+        };
+
+        gestureArea.addEventListener('touchstart', event => {
+            event.preventDefault();
+            for (let i = 0; i < event.changedTouches.length; i++) {
+                console.log(transform);
+                const { identifier, pageX, pageY } = event.changedTouches[i];
+                const [sc, ss, tx, ty] = transform;
+                // Apply the transformation to the pageX/Y.
+                const [x, y, _z] = math.flatten(math.lusolve([
+                    [sc, -ss, tx],
+                    [ss, +sc, ty],
+                    [0, 0, 1],
+                ], [pageX, pageY, 1]));
+                console.log({ x, y, _z });
+                // console.log({x, y});
+                activeGestureStarts.push({ identifier, x, y } as any);
+            }
+        });
         gestureArea.addEventListener('touchmove', event => {
             event.preventDefault();
-            if (1 !== event.touches.length) return;
 
-            // const tl = event.touches[0];
-            // const br = event.touches[1] || { pageX: tl.pageX + 100, pageY: tl.pageY + 100 };
-            const tl = { pageX: 0, pageY: 0 };
-            const br = event.touches[0];
+            if (1 === event.touches.length) {
+                console.assert(1 === activeGestureStarts.length, activeGestureStarts);
+                const s = activeGestureStarts[0];
+                const e = event.touches[0];
 
-            // https://math.stackexchange.com/a/2790865/180371
-            const [sc, ss, tx, ty] = math.multiply(matInv, [tl.pageX, tl.pageY, br.pageX, br.pageY]);
-            console.log({sc, ss, tx, ty});
+                // console.log(math.index([[0], [1], [2]], 0));
+                // https://math.stackexchange.com/a/2790865/180371
+                transform = math.flatten(math.lusolve([
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1],
+                    [s.x, -s.y, 1, 0],
+                    [s.y, +s.x, 0, 1],
+                ], [0, 0, e.pageX, e.pageY])) as number[];
+            }
+            else if (2 === event.touches.length) {
+                const e1 = event.touches[0];
+                const e2 = event.touches[1];
+                const s1 = activeGestureStarts[findActiveGestureIdx(e1)];
+                const s2 = activeGestureStarts[findActiveGestureIdx(e2)];
 
-            imgRef.style.transform = `matrix(${sc}, ${ss}, ${-ss}, ${sc}, ${tx}, ${ty})`;
-            // console.log(out);
+                // https://math.stackexchange.com/a/2790865/180371
+                transform = math.flatten(math.lusolve([
+                    [s1.x, -s1.y, 1, 0],
+                    [s1.y, +s1.x, 0, 1],
+                    [s2.x, -s2.y, 1, 0],
+                    [s2.y, +s2.x, 0, 1],
+                ], [e1.pageX, e1.pageY, e2.pageX, e2.pageY])) as number[];
+            }
+            else {
+
+            }
+
+            {
+                const [sc, ss, tx, ty] = transform;
+                imgRef.style.transform = `matrix(${sc}, ${ss}, ${-ss}, ${sc}, ${tx}, ${ty})`;
+            }
         });
+        const touchEnd = (event: TouchEvent) => {
+            outer:
+            for (let i = 0; i < event.changedTouches.length; i++) {
+                const j = findActiveGestureIdx(event.changedTouches[i])
+                if (0 <= j) {
+                    activeGestureStarts.splice(j, 1);
+                }
+                else {
+                    console.error('Failed to remove!!!');
+                }
+            }
+        };
+        gestureArea.addEventListener('touchcancel', touchEnd);
+        gestureArea.addEventListener('touchend', touchEnd);
     }
 });
